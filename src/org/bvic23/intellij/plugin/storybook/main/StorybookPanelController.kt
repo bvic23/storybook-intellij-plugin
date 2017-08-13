fe94a0d27384b9c5dd06eea4569e6a6848421b7c
bvic23 <bvic23@gmail.com>
2017-08-13 17:10:06 +0200

    Extract state manager

--- src/org/bvic23/intellij/plugin/storybook/main/StorybookPanelController.kt
+++ src/org/bvic23/intellij/plugin/storybook/main/StorybookPanelController.kt
@@ -3,7 +3,7 @@ package org.bvic23.intellij.plugin.storybook.main
 import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
 import com.intellij.ui.content.ContentFactory
-import org.bvic23.intellij.plugin.storybook.main.Status.WAITING_FOR_CONNECTION
+import org.bvic23.intellij.plugin.storybook.main.State.WAITING_FOR_CONNECTION
 import org.bvic23.intellij.plugin.storybook.models.GeneralMessage
 import org.bvic23.intellij.plugin.storybook.models.StoriesArg
 import org.bvic23.intellij.plugin.storybook.models.StorySelection
@@ -15,14 +15,9 @@ import org.bvic23.intellij.plugin.storybook.settings.SettingsManager
 import org.bvic23.intellij.plugin.storybook.socket.SocketClient
 import java.util.*
 import javax.websocket.CloseReason
-import kotlin.concurrent.fixedRateTimer
 import kotlin.concurrent.timerTask
+import org.bvic23.intellij.plugin.storybook.main.State.*
 
-enum class Status {
-    WAITING_FOR_CONNECTION,
-    WAITING_FOR_STORIES,
-    READY
-}
 
 class StorybookPanelController(project: Project) : SettingsChangeNotifier {
     private val panel = StorybookPanel()
@@ -30,16 +25,20 @@ class StorybookPanelController(project: Project) : SettingsChangeNotifier {
     private val socketClient = SocketClient()
     private val notificationManager = NotificationManager()
     private var showFailedMessage = false
-    private var status = WAITING_FOR_CONNECTION
-    private var dots = ""
-    private var dotsTimer: Timer? = null
     private var tree = Tree(emptyList())
     private var selectedStory = StorySelection("", "")
+
     private val treeController = TreeController(panel.storyTree, settingsManager) { storySelection ->
         setCurrentStory(storySelection)
     }
+
     private val filterController = FilterController(panel.filterField, settingsManager) {
         updateTree()
+    }
+
+    private val stateManager = StateManager(panel.statusField, panel.subStatusField) { state ->
+        panel.treePanel.isVisible = state == READY
+        panel.statusPanel.isVisible = state != READY
     }
 
     val content
@@ -48,7 +47,6 @@ class StorybookPanelController(project: Project) : SettingsChangeNotifier {
     init {
         setupMessageBus(project)
         setupListeners(project)
-        setStatus(WAITING_FOR_CONNECTION)
         connect()
     }
 
@@ -81,7 +79,7 @@ class StorybookPanelController(project: Project) : SettingsChangeNotifier {
         socketClient.onClose { reason ->
             if (reason.closeCode == CloseReason.CloseCodes.CLOSED_ABNORMALLY) {
                 notificationManager.info("lost connection, waiting for restart storybook")
-                setStatus(Status.WAITING_FOR_CONNECTION)
+                stateManager.state = WAITING_FOR_CONNECTION
                 connect()
             } else {
                 notificationManager.info("connection got close with reason: $reason")
@@ -93,11 +91,11 @@ class StorybookPanelController(project: Project) : SettingsChangeNotifier {
         }
 
         socketClient.on("setStories") { stories ->
-            setStatus(Status.READY)
             if (stories[0] is StoriesArg) {
                 tree = (stories[0] as StoriesArg).toTree()
                 updateTree()
             }
+            stateManager.state = READY
         }
 
         socketClient.on("getCurrentStory") { _ -> setCurrentStory(selectedStory) }
@@ -105,48 +103,8 @@ class StorybookPanelController(project: Project) : SettingsChangeNotifier {
         socketClient.onOpen {
             notificationManager.info("connected")
             showFailedMessage = false
-            setStatus(Status.WAITING_FOR_STORIES)
-            dots = ""
+            stateManager.state = WAITING_FOR_STORIES
             socketClient.sendText(GeneralMessage("getStories", emptyList()).toMessage())
-        }
-    }
-
-    private fun setStatus(newStatus: Status) {
-        status = newStatus
-        panel.treePanel.isVisible = status == Status.READY
-        panel.statusPanel.isVisible = status != Status.READY
-        updateStatusLabels()
-        if (status != Status.READY) startTimer()
-        else stopTimer()
-    }
-
-    private fun startTimer() {
-        if (dotsTimer != null) return
-        dotsTimer = fixedRateTimer(period = 1000) {
-            dots = if (dots.length < 3) dots + "." else ""
-            updateStatusLabels()
-        }
-    }
-
-    private fun stopTimer() {
-        if (dotsTimer == null) return
-        dotsTimer?.cancel()
-        dotsTimer = null
-        dots = ""
-        updateStatusLabels()
-    }
-
-    private fun updateStatusLabels() {
-        panel.statusField.text = when (status) {
-            Status.WAITING_FOR_CONNECTION -> "Waiting for storybook$dots"
-            Status.WAITING_FOR_STORIES -> "Waiting for story list$dots"
-            Status.READY -> ""
-        }
-
-        panel.subStatusField.text = when (status) {
-            Status.WAITING_FOR_CONNECTION -> "Please start storybook from command line!"
-            Status.WAITING_FOR_STORIES -> "Please refresh your [simu|emu]lator!"
-            Status.READY -> ""
         }
     }
 
